c32d756e1bdbd10e34ffb723e78e0a3aadb51db2
Ayumci <2028daniel01@stu.sbs.ac.th>
2023-11-24 16:24:13 +0700

    feat: upgraded main.py
    Gives description of weather more clearly

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+# Imports
 import requests
-import json
 
-# Define the API endpoint and the API key
+
+# Api
 api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
 api_key = "your_api_key_here"
 
-# Ask the user for the location
-location = input("Please enter the location: ")
 
-# Send a GET request to the API endpoint
-response = requests.get(api_endpoint, params={"q": location, "appid": api_key})
+while True:
 
-# Parse the response JSON
-weather_data = response.json()
+#   Asks user for location
+    location = input("\nPlease enter the location or 'quit' to exit: ")
 
-# Extract and print the weather data
-print("Weather in {}: {} and {}°C".format(location, weather_data["weather"][0]["description"], weather_data["main"]["temp"] - 273.15))
+    if location.lower() == "quit":
+        break
+
+    print(f"\nFetching weather data for {location}...")
+
+#   Fetches weather data from api
+    response = requests.get(api_endpoint, params={"q": location, "appid": api_key})
+    
+#   Checks if the response is valid    
+    if response.status_code == 200:
+
+#       Converts the response to json   
+        weather_data = response.json()
+        
+#       Extract and print the weather data
+        print("Weather in {}: {} and {:.2f}°C".format(
+            location, 
+            weather_data["weather"][0]["description"], 
+            weather_data["main"]["temp"] - 273.15
+        ))
+        print("Humidity: {}%".format(weather_data["main"]["humidity"]))
+        print("Wind Speed: {} m/s".format(weather_data["wind"]["speed"]))
+    else:
+        print("Failed to fetch weather data. Please check your location and try again.")
